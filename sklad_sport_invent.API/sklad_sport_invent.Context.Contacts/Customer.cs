f7a76994acdbbd4363702413c2b9bfc017d62903
danya0934e76 <107752444+danya0934e76@users.noreply.github.com>
2023-10-14 12:17:15 +0300

    Добавил сущность

---
+++ sklad_sport_invent.API/sklad_sport_invent.Context.Contacts/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sklad_sport_invent.Context.Contacts
+{
+    /// <summary>
+    /// покупатель 
+    /// </summary>
+    internal class Customer : BaseAuditEntity
+    {
+        /// <summary>
+        /// Фамилия имя отчество 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// адрес
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// номер телефона клиента  
+        /// </summary>
+        public string TelNum { get; set; }
+
+        /// <summary>
+        /// почта 
+        /// </summary>
+        public  string Email { get; set; }
+    }
+}
---
+++ sklad_sport_invent.API/sklad_sport_invent.Context.Contacts/VedomProductItrem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sklad_sport_invent.Context.Contacts
+{
+    /// <summary>
+    /// ведомость товара
+    /// </summary>
+    internal class VedomProductItrem 
+    {
+        /// <summary>
+        /// идентификтаор 
+        /// </summary>
+       public Guid ID { get; set; }
+        /// <summary>
+        /// идентификтатор товара 
+        /// </summary>
+        public Guid ProductId { get; set; }
+        /// <summary>
+        /// количество  товара 
+        /// </summary>
+        public int Quantity  { get; set; }
+
+        /// <summary>
+        /// сумма
+        /// </summary>
+        public decimal Summ { get; set; }
+    }
+}
