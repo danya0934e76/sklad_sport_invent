27bf8250f9ce647749e173857c5b0ce4eaf1c368
danya0934e76 <107752444+danya0934e76@users.noreply.github.com>
2023-11-11 11:20:16 +0300

    Подготовил все контроллеры

---
+++ sklad_sport_invent.API/sklad_sport_invent.API/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sklad_sport_invent.Context;
+
+namespace sklad_sport_invent.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+public class CustomerController : ControllerBase 
+    {
+        [HttpGet] //localhost:111224/group 
+        public IActionResult GetAllCustomers ()
+        {
+
+            var context = new SkladContext();
+            var customerlist = context.Customers.ToList();
+            
+            return Ok(customerlist);
+
+        }
+        
+
+    }
+    
+}
---
+++ sklad_sport_invent.API/sklad_sport_invent.API/Controllers/SupplierControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sklad_sport_invent.Context;
+
+namespace sklad_sport_invent.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SupplierController : ControllerBase
+    {
+        [HttpGet] //localhost:111224/group 
+        public IActionResult GetAllSupplier()
+        {
+
+            var context = new SkladContext();
+            var supplierlist = context.Suppliers.ToList();
+
+            return Ok(supplierlist);
+
+        }
+
+
+    }
+
+}
